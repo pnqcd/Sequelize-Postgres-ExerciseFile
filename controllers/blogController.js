945b842eac15a6136091380a9b40611e787de625
Quỳnh Phan <vochanhtin139@gmail.com>
2023-11-26 14:54:32 +0700

    browser: categories and tags

---
+++ controllers/blogController.js
@@ -0,0 +1,56 @@
+const controller = {};
+const { create } = require('express-handlebars');
+const models = require('../models');
+
+controller.showList = async (req, res) => {
+    allBlogs = await models.Blog.findAll({
+        attributes: ['id', 'title', 'imagePath', 'summary', 'createdAt'],
+        include: [
+            { model: models.Category },
+            { model: models.Tag },
+            { model: models.Comment }
+        ]
+    });
+
+    categories = await models.Category.findAll({
+        attributes: ['id', 'name']
+    });
+
+    // res.locals.blogs = req.query.category ? allBlogs.filter((item) => item.Category.name == req.query.category) : allBlogs;
+
+    tags = await models.Tag.findAll(( {
+        attributes: ['id', 'name'] 
+    }));
+
+    // res.locals.blogs = req.query.tag ? allBlogs.filter((item) => item.Tags.some(tag => tag.name == req.query.tag)) : allBlogs;
+
+    if (req.query.category) 
+        res.locals.blogs = allBlogs.filter((item) => item.Category.name == req.query.category);
+    else if (req.query.tag) 
+        res.locals.blogs = allBlogs.filter((item) => item.Tags.some(tag => tag.name == req.query.tag));
+    else res.locals.blogs = allBlogs;
+
+    res.locals.sidebarData = {
+        categories,
+        tags,
+    };
+
+    res.render('index');
+}
+
+controller.showDetails = async(req, res) => {
+    let id = isNaN(req.params.id) ? 0 : parseInt(req.params.id);
+    res.locals.blog = await models.Blog.findOne({
+        attributes: ['id', 'title', 'description', 'createdAt'],
+        where: { id: id },
+        include: [
+            { model: models.Category },
+            { model: models.User },
+            { model: models.Tag },
+            { model: models.Comment }
+        ]
+    })
+    res.render('details');
+}
+
+module.exports = controller;
---
+++ routes/blogRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/blogController');
+
+router.get('/', controller.showList);
+router.get('/:id', controller.showDetails);
+
+module.exports = router;
